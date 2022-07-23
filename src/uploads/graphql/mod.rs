bf79a38d805eaaddbd9875326adf41088227ac41
Victor Quiroz <git@victorhqc.com>
2022-07-23 22:33:34 +0200

    feat: show file url

--- src/uploads/graphql/mod.rs
+++ src/uploads/graphql/mod.rs
@@ -7,7 +7,7 @@ pub use query::*;
 use async_graphql::{SimpleObject, ID};
 use chrono::{DateTime, Utc};
 use entity::uploads;
-use std::convert::From;
+use std::{convert::From, env};
 use uuid::Uuid;
 
 #[derive(SimpleObject, Clone)]
@@ -19,11 +19,23 @@ pub struct FileUpload {
     pub updated_at: DateTime<Utc>,
     // #[graphql(skip)]
     // created_by: String,
+    pub urls: Urls,
+}
+
+#[derive(SimpleObject, Clone)]
+pub struct Urls {
+    url: String,
 }
 
 impl From<uploads::Model> for FileUpload {
     fn from(file_upload: uploads::Model) -> Self {
         let uuid = Uuid::from_bytes(file_upload.uuid());
+
+        let api_url = env::var("API_URL").expect("API_URL is not set");
+        let urls = Urls {
+            url: format!("{}/file/{}", api_url, uuid),
+        };
+
         // let created_by = Uuid::from_bytes(file_upload.created_by());
 
         FileUpload {
@@ -32,7 +44,7 @@ impl From<uploads::Model> for FileUpload {
             content_type: file_upload.content_type,
             created_at: file_upload.created_at,
             updated_at: file_upload.updated_at,
-            // created_by: created_by.to_string(),
+            urls, // created_by: created_by.to_string(),
         }
     }
 }
